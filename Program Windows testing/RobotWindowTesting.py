bd9bf469a2c00d85637734fc2a1361510142a12f
aduxbury0 <aduxbury0@hotmail.co.uk>
2016-11-17 18:17:18 +0000

    Final version of the main robot window

--- Program Windows testing/RobotWindowTesting.py
+++ Program Windows testing/RobotWindowTesting.py
@@ -15,39 +15,40 @@ mainFrame.pack()
 # This is the left hand side canvas for the robot to move on
 robotcanvas = Canvas(mainFrame, width=500, height=400, bg="grey")
 robotcanvas.pack(side="left")
+# Initial drawing of the robot block
 robot1 = robotcanvas.create_rectangle(3, 7, 3 + 10, 7 + 10)
 
-# This is the top half of the control inputs, containing the 2 input boxes and buttons
-controlFrame = Frame(mainFrame, width=300, height=400, bg="white", pady="10")
+# This is the frame that contains all of the control inputs on the right hand side of the screen
+controlFrame = Frame(mainFrame, width=300, height=400, bg="gray90", pady="10")
 controlFrame.pack_propagate(0)
 controlFrame.pack(side="right")
 
-# This is the bottom half of the control inputs, containing the start/stop buttons and the label containing the distance
-inputsFrame = Frame(controlFrame, width=300, height=200, bg="white")
+# This is the top half of the control inputs, containing the 2 input boxes and buttons
+inputsFrame = Frame(controlFrame, width=300, height=200, bg="gray90")
 inputsFrame.pack_propagate(0)
 inputsFrame.pack(side="top")
 
-distanceCounterFrame = Frame(controlFrame, width=300, height=200, bg="white", bd="5")
+# This is the bottom half of the control inputs, containing the start/stop buttons and the label containing the distance
+distanceCounterFrame = Frame(controlFrame, width=300, height=200, bg="gray90", bd="5")
 distanceCounterFrame.pack_propagate(0)
 distanceCounterFrame.pack(side="bottom")
 
 robotSpeedInput = Entry(inputsFrame)
-robotSpeedInput.grid(row="1", column="0")
+robotSpeedInput.grid(row="1", column="0", padx="5")
 robotSpeedButton = Button(inputsFrame, text="Set Speed")
-robotSpeedButton.grid(row="1", column="1")
+robotSpeedButton.grid(row="1", column="1", sticky="W", padx="5", pady="5")
 setDistanceInput = Entry(inputsFrame)
-setDistanceInput.grid(row="4", column="0")
+setDistanceInput.grid(row="2", column="0", padx="5")
 distanceInputButton = Button(inputsFrame, text="Set Distance")
-distanceInputButton.grid(row="4", column="1")
+distanceInputButton.grid(row="2", column="1", padx="5", pady="5")
 
-startButton = Button(distanceCounterFrame, text="Start")
-
-startButton.grid(row="0", column="0")
-stopButton = Button(distanceCounterFrame, text="Stop")
-stopButton.grid(row="0", column="1")
-distanceLabel = Label(distanceCounterFrame, text="The robot is \n"+ str(robotDistance)+"\n blocks from the end", pady="10", bg="white")
-distanceLabel.config(font=("Calibri", 13), relief="sunken", padx="4")
-distanceLabel.grid(row="5", column="0", columnspan="2")
+startButton = Button(distanceCounterFrame, text="Start", padx="10", pady="5")
+startButton.grid(row="0", column="0", pady="15")
+stopButton = Button(distanceCounterFrame, text="Stop", padx="10", pady="5")
+stopButton.grid(row="0", column="1", pady="15")
+distanceLabel = Label(distanceCounterFrame, text="The robot is\n" + str(robotDistance)+"\n  blocks from the end  ", pady="10", bg="white")
+distanceLabel.config(font=("Arial", 13), relief="groove")
+distanceLabel.grid(row="2", column="0", columnspan="2")
 
 root.mainloop()
 
